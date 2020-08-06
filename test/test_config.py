408873898d0cda6bfb660db9a0bef0bb20416e40
Abe Winter <abe-winter@users.noreply.github.com>
2020-08-06 19:32:36 -0400

    update tests

--- test/test_config.py
+++ test/test_config.py
@@ -5,11 +5,11 @@ def test_render():
   config.render_config()
 
 def test_query_render_dialect():
-  assert config.QueryRenderer({}, {}, 'postgres').wildcard == '%s'
-  assert config.QueryRenderer({}, {}, 'sqlite').wildcard == '?'
+  assert config.QueryRenderer('postgres').wildcard == '%s'
+  assert config.QueryRenderer('sqlite').wildcard == '?'
 
 def test_query_render_dialect():
-  render = config.QueryRenderer({'c1': 'C1', 'c2': 'C2'}, {'tab': 'TAB'}, 'postgres')
-  assert render.select('tab', ('c1',), 'c2') == "select C1 from TAB\nwhere C2 = %s"
-  assert render.update('tab', 'c1', 'c2') == "update TAB\nset C1 = %s\nwhere C2 = %s"
-  assert render.insert('tab', ('c1', 'c2')) == "insert into TAB (C1, C2)\nvalues (%s, %s)"
+  render = config.QueryRenderer('postgres')
+  assert render.select('tab', ('c1',), 'c2') == "select c1 from tab where c2 = %s"
+  assert render.update('tab', 'c1', 'c2') == "update tab set c1 = %s where c2 = %s"
+  assert render.insert('tab', ('c1', 'c2')) == "insert into tab (c1, c2) values (%s, %s)"
